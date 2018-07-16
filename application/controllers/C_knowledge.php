02d06ed55af5c1a597a6467d3d9b25874342bb1b
Magrid Prisca Juwita <magridprisca@gmail.com>
2018-07-16 20:05:45 +0700

    knowledge

---
+++ application/controllers/C_knowledge.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class C_knowledge extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('M_knowledge');
+		$this->load->helper('url_helper');
+	}
+	public function add(){
+		$data['menu']='knowledge';
+
+    $this->load->view($_SESSION['level'].'/V_header_'.$_SESSION['level'],$data);
+    $this->load->view('umum/V_addKnowledge');
+    $this->load->view('umum/V_footer');
+	}
+}
+?>
