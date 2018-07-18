ed0928466d5878da2849ccc3cec65b81e5a121cf
Magrid Prisca Juwita <magridprisca@gmail.com>
2018-07-18 14:44:11 +0700

    baru

--- application/controllers/C_knowledge.php
+++ application/controllers/C_knowledge.php
@@ -4,22 +4,94 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class C_knowledge extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
+		$this->load->model('M_division');
 		$this->load->model('M_knowledge');
 		$this->load->helper('url_helper');
 	}
 	public function add(){
-		$data['menu']='knowledge';
+		$this->form_validation->set_rules('title', 'Full Name', 'required');
+		$this->form_validation->set_rules('category', 'Username', 'required');
+		$this->form_validation->set_rules('divisi', 'Email', 'required');
+		$this->form_validation->set_rules('tipe', 'Password', 'required');
+		$this->form_validation->set_rules('description', 'Repeat Password', 'required');
+		$this->form_validation->set_rules('data', 'data', '');
 
-    $this->load->view($_SESSION['level'].'/V_header_'.$_SESSION['level'],$data);
-    $this->load->view('umum/V_addKnowledge');
-    $this->load->view('umum/V_footer');
-	}
-	public function save(){
+		if ($this->form_validation->run() == FALSE){
+			$data['menu']='knowledge';
+			$data['divisi']=$this->M_division->getAll();
+
+	    $this->load->view($_SESSION['level'].'/V_header_'.$_SESSION['level'],$data);
+	    $this->load->view('umum/V_addKnowledge',$data);
+	    $this->load->view('umum/V_footer');
+		}
+		else {
+
+			$target_dir = "assets/upload/";
+			$target_file = $target_dir . basename($_FILES["data"]["name"]);
+			$uploadOk = 1;
+			$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+			// Check if image file is a actual image or fake image
+			$check = getimagesize($_FILES["data"]["tmp_name"]);
+			if($check !== false) {
+				echo "File is an image - " . $check["mime"] . ".";
+				$uploadOk = 1;
+			} else {
+				echo "File is not an image.";
+				$uploadOk = 0;
+			}
+			// Check if file already exists
+			if (file_exists($target_file)) {
+				echo "Sorry, file already exists.";
+				$uploadOk = 0;
+			}
+			// Check file size
+			if ($_FILES["data"]["size"] > 500000) {
+				echo "Sorry, your file is too large.";
+				$uploadOk = 0;
+			}
+			// Allow certain file formats
+			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+			&& $imageFileType != "gif" ) {
+				echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+				$uploadOk = 0;
+			}
+			// Check if $uploadOk is set to 0 by an error
+			if ($uploadOk == 0) {
+				echo "Sorry, your file was not uploaded.";
+			// if everything is ok, try to upload file
+			} else {
+				if (move_uploaded_file($_FILES["data"]["tmp_name"], $target_file)) {
+					echo "The file ". basename( $_FILES["data"]["name"]). " has been uploaded.";
+				} else {
+					echo "Sorry, there was an error uploading your file.";
+				}
+			}
+			if($uploadOk== 1){
+			$data = array(
+				'title'					=> set_value('title'),
+				'type'					=> 0,
+				'category'			=> set_value('category'),
+				'divisionID'		=> set_value('divisi'),
+				'file'					=> $target_file,
+				'fileType'			=> set_value('tipe'),
+				'description'		=> set_value('description'),
+				'dateOfUpload'	=> date("Y-m-d h:i:sa"),
+				'userID'				=> $_SESSION['user'],
+				'totalRate'			=> $target_file
+			);
+			$res=$this->M_knowledge->create($data);
+			redirect(base_url($_SESSION['level']));
+			}
+
+		}
+
 
 	}
 
 	public function read(){
 		$data['menu']='dashboard';
+
+		$data['knowledge']=$this->M_knowledge->getAll();
 		$this->load->view($_SESSION['level'].'/V_header_'.$_SESSION['level'],$data);
 		$this->load->view('umum/V_files');
 		$this->load->view('umum/V_footer');
--- application/views/umum/V_addKnowledge.php
+++ application/views/umum/V_addKnowledge.php
@@ -1,86 +1,86 @@
 <div class="row">
-          <div class="col-lg-12">
-            <h3 class="page-header"><i class="fa fa-files-o"></i> Form Validation</h3>
-            <ol class="breadcrumb">
-              <li><i class="fa fa-home"></i><a href="<?= base_url($_SESSION['level']);?>">Home</a></li>
-              <li><i class="icon_document_alt"></i>Knowledge</li>
-            </ol>
-          </div>
-        </div>
-        <!-- Form validations -->
-        <div class="row">
-          <div class="col-lg-12">
-            <section class="panel">
-              <header class="panel-heading">
-                Form validations
-              </header>
-              <div class="panel-body">
-                <div class="form">
-                  <form class="form-validate form-horizontal" id="feedback_form" method="get" action="">
-                    <div class="form-group ">
-                      <label for="cname" class="control-label col-lg-2">Title <span class="required">*</span></label>
-                      <div class="col-lg-10">
-                        <input class="form-control" id="title" name="title" minlength="5" type="text" required />
-                      </div>
-                    </div>
-                    <div class="form-group ">
-                      <label for="cemail" class="control-label col-lg-2">Category <span class="required">*</span></label>
-                      <div class="col-lg-10">
-                        <div class="radio">
-                          <label><input type="radio" name="category" value="0">Engginering</label>
-                        </div>
-                        <div class="radio">
-                          <label><input type="radio" name="category" value="1">Non-Engginering</label>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label for="divi" class="control-label col-lg-2">Division<span class="required">*</span></label>
-                      <div class="col-lg-10">
-                      <select class="form-control m-bot15" name="divisi">
-                          <option>Choose Your Division</option>
-                          <?php foreach($divisi as $div) { ?>
-                            <option value="<?= $div->divisionID ?>"><?= $div->divisionName ?></option>
-                          <?php } ?>
-                      </select>
-                    </div>
-                    </div>
-                    <div class="form-group ">
-                      <label for="cemail" class="control-label col-lg-2">File Type <span class="required">*</span></label>
-                      <div class="col-lg-10">
-                        <div class="radio">
-                          <label><input type="radio" name="tipe">PDF File</label>
-                        </div>
-                        <div class="radio">
-                          <label><input type="radio" name="tipe">Video</label>
-                        </div>
-                        <div class="radio">
-                          <label><input type="radio" name="tipe">Foto</label>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label for="cname" class="control-label col-lg-2">File knowledge <span class="required">*</span></label>
-                      <div class="col-lg-10">
-                        <input class="form-control" type="file" id="exampleInputFile" name="file">
-                      </div>
-                    </div>
-                    <div class="form-group ">
-                      <label for="ccomment" class="control-label col-lg-2">Description</label>
-                      <div class="col-lg-10">
-                        <textarea class="form-control " id="description" name="description" required></textarea>
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <div class="col-lg-offset-2 col-lg-10">
-                        <button class="btn btn-primary" type="submit">Save</button>
-                        <button class="btn btn-default" type="button">Cancel</button>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-
+  <div class="col-lg-12">
+    <h3 class="page-header"><i class="fa fa-files-o"></i> Form Validation</h3>
+    <ol class="breadcrumb">
+      <li><i class="fa fa-home"></i><a href="<?= base_url($_SESSION['level']);?>">Home</a></li>
+      <li><i class="icon_document_alt"></i>Knowledge</li>
+    </ol>
+  </div>
+</div>
+<!-- Form validations -->
+<div class="row">
+  <div class="col-lg-12">
+    <section class="panel">
+      <header class="panel-heading">
+        Form validations
+      </header>
+      <div class="panel-body">
+        <div class="form">
+          <form class="form-validate form-horizontal" id="feedback_form" method="post" action="<?= base_url('C_knowledge/add')?>"  enctype="multipart/form-data">
+            <div class="form-group ">
+              <label for="title" class="control-label col-lg-2">Title <span class="required">*</span></label>
+              <div class="col-lg-10">
+                <input class="form-control" id="title" name="title" minlength="5" type="text" required />
               </div>
-            </section>
-          </div>
+            </div>
+            <div class="form-group ">
+              <label for="cemail" class="control-label col-lg-2">Category <span class="required">*</span></label>
+              <div class="col-lg-10">
+                <div class="radio">
+                  <label><input type="radio" name="category" value="0">Engginering</label>
+                </div>
+                <div class="radio">
+                  <label><input type="radio" name="category" value="1">Non-Engginering</label>
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="divi" class="control-label col-lg-2">Division<span class="required">*</span></label>
+              <div class="col-lg-10">
+              <select class="form-control m-bot15" name="divisi">
+                  <option>Choose Your Division</option>
+                  <?php foreach($divisi as $div) { ?>
+                    <option value="<?= $div->divisionID ?>"><?= $div->divisionName ?></option>
+                  <?php } ?>
+              </select>
+            </div>
+            </div>
+            <div class="form-group ">
+              <label for="cemail" class="control-label col-lg-2">File Type <span class="required">*</span></label>
+              <div class="col-lg-10">
+                <div class="radio">
+                  <label><input type="radio" name="tipe" id="tipe">PDF File</label>
+                </div>
+                <div class="radio">
+                  <label><input type="radio" name="tipe" id="tipe">Video</label>
+                </div>
+                <div class="radio">
+                  <label><input type="radio" name="tipe" id="tipe">Foto</label>
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="cname" class="control-label col-lg-2">File knowledge <span class="required">*</span></label>
+              <div class="col-lg-10">
+                <input class="form-control" type="file" id="InputFile" name="data" accept="">
+              </div>
+            </div>
+            <div class="form-group ">
+              <label for="description" class="control-label col-lg-2">Description</label>
+              <div class="col-lg-10">
+                <textarea class="form-control " id="description" name="description" required></textarea>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-lg-offset-2 col-lg-10">
+                <button class="btn btn-primary" type="submit">Save</button>
+                <button class="btn btn-default" type="button">Cancel</button>
+              </div>
+            </div>
+          </form>
         </div>
+
+      </div>
+    </section>
+  </div>
+</div>
