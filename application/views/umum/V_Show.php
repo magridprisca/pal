33d31dca3d2734e8950adeee98df2dc19e293096
Magrid Prisca Juwita <magridprisca@gmail.com>
2018-08-24 15:34:48 +0700

    dikit

--- application/views/umum/V_Show.php
+++ application/views/umum/V_Show.php
@@ -3,7 +3,7 @@
     <h3 class="page-header"><i class="icon_document_alt"></i>knowledge</h3>
     <ol class="breadcrumb">
       <li><i class="fa fa-home"></i><a href="<?php echo base_url($_SESSION['level']) ?>">Home</a></li>
-      <li><i class="icon_document_alt"></i><a href="<?php echo base_url('C_knowledge/read/'.$knowledge->divisionID) ?>"><?php echo ucwords($knowledge->divisionName); ?></a></li>
+      <li><i class="icon_document_alt"></i><a href="<?php echo base_url('C_knowledge/read/'.$divi->divisionID) ?>"><?php echo ucwords($divi->divisionName); ?></a></li>
       <li><i class="fa fa-paperclip"></i>Details</li>
     </ol>
   </div>
